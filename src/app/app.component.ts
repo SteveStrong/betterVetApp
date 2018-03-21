6df16a11dbc3ef461b3836c3d909473e13572a7f
vernrwalker <Vern.R.Walker@gmail.com>
2018-03-21 12:01:06 -0400

    Data Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'the better Vet App';
+  data = {};
+  list = ['Tobias', 'Lina'];
+  vetpara = [
+    {
+      evidence: 'The Board acknowledges that the record includes diagnoses of PTSD.'
+    },
+    {
+      reasoning: 'The records reflecting diagnoses of PTSD do not reveal an explanation as to how the Veteran met the criteria for PTSD, however; as such, the Board finds the diagnoses have limited probative value.'
+    },
+    {
+      reasoning: `In contrast, the Board finds the VA examiners' and other medical professionals' determinations that the Veteran does not have PTSD are highly probative as the determinations are supported by detailed explanation for why the diagnosis was not proper and are based on findings consistent with the medical records.`
+    },
+    {
+      reasoning: 'The Board finds the probative value of the findings of no PTSD outweighs the probative value of the findings of PTSD.'
+    },
+    {
+      finding: 'Thus, the Board finds the Veteran does not have PTSD.'
+    }
+  ]
+
 }
