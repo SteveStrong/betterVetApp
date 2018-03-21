b9877375d7404309bd70f1e0ee560cf3435ac4e3
vernrwalker <Vern.R.Walker@gmail.com>
2018-03-21 12:17:20 -0400

    restructured json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,19 +11,24 @@ export class AppComponent {
   list = ['Tobias', 'Lina', 'Eli'];
   vetpara = [
     {
-      evidence: 'The Board acknowledges that the record includes diagnoses of PTSD.'
+      type: 'evidence',
+      text: 'The Board acknowledges that the record includes diagnoses of PTSD.'
     },
     {
-      reasoning: 'The records reflecting diagnoses of PTSD do not reveal an explanation as to how the Veteran met the criteria for PTSD, however; as such, the Board finds the diagnoses have limited probative value.'
+      type: 'reasoning',
+      text: 'The records reflecting diagnoses of PTSD do not reveal an explanation as to how the Veteran met the criteria for PTSD, however; as such, the Board finds the diagnoses have limited probative value.'
     },
     {
-      reasoning: `In contrast, the Board finds the VA examiners' and other medical professionals' determinations that the Veteran does not have PTSD are highly probative as the determinations are supported by detailed explanation for why the diagnosis was not proper and are based on findings consistent with the medical records.`
+      type: 'reasoning',
+      text: `In contrast, the Board finds the VA examiners' and other medical professionals' determinations that the Veteran does not have PTSD are highly probative as the determinations are supported by detailed explanation for why the diagnosis was not proper and are based on findings consistent with the medical records.`
     },
     {
-      reasoning: 'The Board finds the probative value of the findings of no PTSD outweighs the probative value of the findings of PTSD.'
+      type: 'reasoning',
+      text: 'The Board finds the probative value of the findings of no PTSD outweighs the probative value of the findings of PTSD.'
     },
     {
-      finding: 'Thus, the Board finds the Veteran does not have PTSD.'
+      type: 'finding',
+      text: 'Thus, the Board finds the Veteran does not have PTSD.'
     }
   ]
 
